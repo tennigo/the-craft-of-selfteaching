efa7ae11f19d2b21c77184ba9aedd9e56fc888ab
Tenni_Go <tennigo@gmail.com>
2019-06-26 10:10:36 +0800

    2019-06-26

---
+++ my-notes/practices/continue.py
@@ -0,0 +1,24 @@
+# for n in range(2, 100):
+# 	if n == 2:
+# 		print(n)
+# 		continue
+# 	for i in range(2, n):
+# 		if (n % i) == 0:
+# 			break
+# 	else:
+# 		print(n)
+
+for n in range(2,10):
+	if n == 7:
+		print(n)
+		break
+	else:
+		print(f'{n}')
+
+print('---')
+
+for n in range(2, 10):
+	print(f'{n}')
+	if n == 7:
+		print(n)
+		break
