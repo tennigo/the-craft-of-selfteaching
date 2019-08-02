f5f3dc5ae79215b282af615f7a426741c3b8d6fe
Tenni_Go <tennigo@gmail.com>
2019-08-02 13:28:48 +0800

    20190802

---
+++ my-notes/self-practices/is_leap.py
@@ -0,0 +1,9 @@
+def is_leap(year):
+	"""
+	input a int, check if is a leap year.
+	param: year: an int
+	return: True:if input is a leap year
+			False: if input is not a leap year.
+	"""
+	n = int(year)
+	if n % 4 == 0
